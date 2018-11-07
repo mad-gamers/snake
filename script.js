7a580251dd8e66b0c24df2d7919f35ca5f6f0eed
Lera Green <zelnayavaleriya@gmail.com>
2018-11-08 00:12:49 +0300

    Adds game loop: snake movement, apples generation

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const canvas = document.querySelector(`canvas`);
 const ctx = canvas.getContext(`2d`);
 
+const getRandomRange = (min, max) => {
+  return min + Math.floor(Math.random() * (max - min + 1));
+};
+
 const gameCanvas = {
   color: `green`,
   x: 0,
@@ -9,49 +13,80 @@ const gameCanvas = {
   height: 300
 };
 
-const render = (item) => {
-  ctx.fillStyle = item.color;
-  ctx.fillRect(item.x, item.y, item.width, item.height);
-};
-
-render(gameCanvas);
-
-const item = {
+const initialItem = {
   color: `black`,
   parts: 1,
   x: 10,
   y: 10,
   width: 10,
-  height: 10
+  height: 10,
+  direction: `down`
 };
 
-const clear = () => {
-  ctx.clearRect(gameCanvas.x, gameCanvas.y, gameCanvas.width, gameCanvas.height);
+const apple = {
+  color: `orange`,
+  width: 10,
+  height: 10,
+  x: getRandomRange(0, 29) * 10,
+  y: getRandomRange(0, 29) * 10
+};
+
+const render = (item) => {
+  ctx.fillStyle = item.color;
+  ctx.fillRect(item.x, item.y, item.width, item.height);
+};
+
+const clear = (item) => {
+  ctx.clearRect(item.x, item.y, item.width, item.height);
+};
+
+const isAppleEaten = () => {
+  return (initialItem.x === apple.x && initialItem.y === apple.y);
+};
+
+const nextStep = () => {
+  clear(gameCanvas);
   render(gameCanvas);
-};
+  render(apple);
+  render(initialItem);
 
-render(item);
+  if (initialItem.direction === `right`) {
+    initialItem.x = initialItem.x + 10;
+  } else if (initialItem.direction === `left`) {
+    initialItem.x = initialItem.x - 10;
+  } else if (initialItem.direction === `up`) {
+    initialItem.y = initialItem.y - 10;
+  } else if (initialItem.direction === `down`) {
+    initialItem.y = initialItem.y + 10;
+  }
+
+  if (isAppleEaten()) {
+    generateApple();
+  }
+  setTimeout(() => {
+    nextStep();
+  }, 100);
+};
 
 const checkKey = (key) => {
-  if (key === `ArrowRight` && item.x + 10 !== gameCanvas.width) {
-    clear(item);
-    item.x = item.x + 10;
-    render(item);
-  } else if (key === `ArrowDown` && item.y + 10 !== gameCanvas.height) {
-    clear(item);
-    item.y = item.y + 10;
-    render(item);
-  } else if (key === `ArrowUp` && !!item.y) {
-    clear(item);
-    item.y = item.y - 10;
-    render(item);
-  } else if (key === `ArrowLeft` && !!item.x) {
-    clear(item);
-    item.x = item.x - 10;
-    render(item);
+  if (key === `ArrowRight`) {
+    initialItem.direction = `right`;
+  } else if (key === `ArrowDown`) {
+    initialItem.direction = `down`;
+  } else if (key === `ArrowUp`) {
+    initialItem.direction = `up`;
+  } else if (key === `ArrowLeft`) {
+    initialItem.direction = `left`;
   }
+};
+
+const generateApple = () => {
+  apple.x = getRandomRange(0, 29) * 10;
+  apple.y = getRandomRange(0, 29) * 10;
 };
 
 document.addEventListener(`keydown`, (evt) => {
   checkKey(evt.key);
 });
+
+nextStep();
