15d7bbaab9ea8195542955e4dd6648af1127db6a
Lera Green <zelnayavaleriya@gmail.com>
2019-02-13 00:56:04 +0300

    adds new algoritm

--- script.js
+++ script.js
@@ -5,107 +5,62 @@
 // TODO сделать красиво
 // TODO добавить глаза
 // TODO как сделать с setTimeout
-// TODO из-за сайд-ээфекта на запрет смены направления на противоположное не даёт со старта поехать назад
-
-const canvas = document.querySelector(`canvas`);
-const ctx = canvas.getContext(`2d`);
-let timer = null;
+// TODO из-за сайд-эффекта на запрет смены направления на противоположное не даёт со старта поехать назад
+// TODO Яблоко не должно появляться там же, где змейка 0_o
 
 const getRandomRange = (min, max) => {
   return min + Math.floor(Math.random() * (max - min + 1));
 };
 
-const game = {
-  points: 0,
-  lives: 1
-};
-
-const gameCanvas = {
+const gameField = {
   color: `green`,
   x: 0,
   y: 0,
-  width: 840,
-  height: 840,
-  step: 20,
+  width: 800,
+  height: 800,
+  step: 20
 };
 
 const border = {
-  size: gameCanvas.step,
+  size: gameField.step,
   color: `blue`
 };
 
 const snake = {
   color: `black`,
   direction: null,
+  bodyColor: `black`,
+  headColor: `red`,
   parts: [
     {
       x: 200,
-      y: 20,
-      color: `red`
+      y: 20
     },
     {
-      x: 180,
-      y: 20,
-      color: `black`
-    },
-    {
-      x: 160,
-      y: 20,
-      color: `black`
-    },
-    {
-      x: 140,
-      y: 20,
-      color: `black`
-    },
-    {
-      x: 120,
-      y: 20,
-      color: `black`
-    },
-    {
-      x: 100,
-      y: 20,
-      color: `black`
-    },
-    {
-      x: 80,
-      y: 20,
-      color: `black`
-    },
-    {
-      x: 60,
-      y: 20,
-      color: `black`
-    },
-    {
-      x: 40,
-      y: 20,
-      color: `black`
+      x: 200,
+      y: 0
     }
   ]
 };
 
+const canvas = document.querySelector(`canvas`);
+canvas.width = `${gameField.width}`;
+canvas.height = `${gameField.height}`;
+canvas.style.border = `solid ${border.size}px ${border.color}`;
+const ctx = canvas.getContext(`2d`);
+let timer = null;
+
+
 const apple = {
   color: `orange`,
-  width: gameCanvas.step,
-  height: gameCanvas.step,
-  x: getRandomRange(1, gameCanvas.width / gameCanvas.step - 1) * gameCanvas.step,
-  y: getRandomRange(1, gameCanvas.height / gameCanvas.step - 1) * gameCanvas.step
+  width: gameField.step,
+  height: gameField.step,
+  x: getRandomRange(0, gameField.width / gameField.step - 1) * gameField.step,
+  y: getRandomRange(0, gameField.height / gameField.step - 1) * gameField.step
 };
 
 const startGame = () => {
   timer = setInterval(nextStep, 200);
-};
-
-const overGame = () => {
-  game.lives -= 1;
-  if (timer) {
-    clearInterval(timer);
-    timer = null;
-    snake.direction = null;
-    console.log(`Конец игры`);
-  }
 };
 
 const render = (item) => {
@@ -113,22 +68,26 @@ const render = (item) => {
   ctx.fillRect(item.x, item.y, item.width, item.height);
 };
 
-const renderSnake = () => {
-  // ctx.fillStyle = snake.color;
+const renderSnake = (_snake) => {
+  ctx.fillStyle = _snake.bodyColor;
 
-  for (const part of snake.parts) {
-    ctx.fillStyle = part.color;
-    ctx.fillRect(part.x, part.y, gameCanvas.step, gameCanvas.step);
-  }
+  snake.parts.forEach((element, index) => {
+    if (index === 0) {
+      ctx.fillStyle = _snake.headColor;
+    } else {
+      ctx.fillStyle = _snake.bodyColor;
+    }
+    ctx.fillRect(element.x, element.y, gameField.step, gameField.step);
+  });
 };
 
 const clear = (item) => {
   ctx.clearRect(item.x, item.y, item.width, item.height);
 };
 
-const generateApple = () => {
-  apple.x = getRandomRange(1, gameCanvas.width / gameCanvas.step - 1) * gameCanvas.step;
-  apple.y = getRandomRange(1, gameCanvas.height / gameCanvas.step - 1) * gameCanvas.step;
+const generateApple = (apple) => {
+  apple.x = getRandomRange(0, gameField.width / gameField.step - 1) * gameField.step;
+  apple.y = getRandomRange(0, gameField.height / gameField.step - 1) * gameField.step;
 };
 
 const isAppleEaten = () => {
@@ -138,18 +97,22 @@ const isAppleEaten = () => {
   return (firstPartX === apple.x && firstPartY === apple.y);
 };
 
-const addNewSnakePart = () => {
+const addNewSnakePart = (snake) => {
   const lastPart = snake.parts[snake.parts.length - 1];
   const newPart = Object.assign({}, lastPart);
 
   if (snake.direction === `right`) {
-    newPart.x = lastPart.x - gameCanvas.step;
+    newPart.x = lastPart.x - gameField.step;
+    newPart.y += 0;
   } else if (snake.direction === `left`) {
-    newPart.x = lastPart.x + gameCanvas.step;
+    newPart.x = lastPart.x + gameField.step;
+    newPart.y += 0;
   } else if (snake.direction === `up`) {
-    newPart.y = lastPart.y + gameCanvas.step;
+    newPart.y = lastPart.y + gameField.step;
+    newPart.x += 0;
   } else if (snake.direction === `down`) {
-    newPart.y = lastPart.y - gameCanvas.step;
+    newPart.y = lastPart.y - gameField.step;
+    newPart.x += 0;
   }
 
   snake.parts.push(newPart);
@@ -159,140 +122,46 @@ const addNewSnakePart = () => {
 // TODO мэйби есть смысл перебирать с конца, потому что мы врезаемся в хвост
 // TODO если я нажимаю лево из право тоже считает ошибкой. Почему?
 
-const checkBumpIntoTail = (direction) => {
-  const parts = snake.parts;
-  const firstPart = parts[0];
-  let isTailNear = false;
-
-  if (direction === `left`) {
-    isTailNear = parts.some((part) => part.x === firstPart.x - gameCanvas.step && part.y === firstPart.y);
-  } else if (direction === `right`) {
-    isTailNear = parts.some((part) => part.x === firstPart.x + gameCanvas.step && part.y === firstPart.y);
-  } else if (direction === `down`) {
-    isTailNear = parts.some((part) => part.y === firstPart.y + gameCanvas.step && part.x === firstPart.x);
-  } else if (direction === `up`) {
-    isTailNear = parts.some((part) => part.y === firstPart.y - gameCanvas.step && part.x === firstPart.x);
+const snakeMove = (direction) => {
+  if (direction === null) {
+    return;
   }
-
-  return isTailNear;
-};
-
-const checkBumpIntoWall = (direction) => {
-  const parts = snake.parts;
-  const firstPart = parts[0];
-  let isWallNear = false;
-  if (direction === `right`) {
-    isWallNear = firstPart.x + gameCanvas.step === gameCanvas.width;
-  } else if (direction === `left`) {
-    isWallNear = firstPart.x - gameCanvas.step === 0;
-  } else if (direction === `up`) {
-    isWallNear = firstPart.y - gameCanvas.step === 0;
-  } else if (direction === `down`) {
-    isWallNear = firstPart.y + gameCanvas.step === gameCanvas.height;
-  }
-
-  return isWallNear;
-};
-
-const moveRight = (firstPart, lastPart, firstPartX, firstPartY) => {
-
-  if (lastPart.y !== firstPartY) {
-    lastPart.y = firstPartY;
-  }
-
-  if (firstPartX + gameCanvas.step !== snake.parts[1].x) {
-    lastPart.x = firstPartX + gameCanvas.step;
-    lastPart.color = `red`;
-    firstPart.color = `black`;
-    snake.parts.unshift(snake.parts.pop());
-  }
-};
-
-const moveDown = (firstPart, lastPart, firstPartX, firstPartY) => {
-  if (firstPartY + gameCanvas.step !== snake.parts[1].y) {
-    lastPart.x = firstPartX;
-    lastPart.y = firstPartY + gameCanvas.step;
-    lastPart.color = `red`;
-    firstPart.color = `black`;
-    snake.parts.unshift(snake.parts.pop());
-  }
-};
-
-const moveLeft = (firstPart, lastPart, firstPartX, firstPartY) => {
-  if (lastPart.y !== firstPartY) {
-    lastPart.y = firstPartY;
-  }
-
-  if (firstPartX - gameCanvas.step !== snake.parts[1].x) {
-    lastPart.x = firstPartX - gameCanvas.step;
-    lastPart.color = `red`;
-    firstPart.color = `black`;
-    snake.parts.unshift(snake.parts.pop());
-  }
-};
-
-const moveUp = (firstPart, lastPart, firstPartX, firstPartY) => {
-  if (firstPartY - gameCanvas.step !== snake.parts[1].y) {
-    lastPart.x = firstPartX;
-    lastPart.y = firstPartY - gameCanvas.step;
-    lastPart.color = `red`;
-    firstPart.color = `black`;
-    snake.parts.unshift(snake.parts.pop());
-  }
-};
-
-const checkSnakeMove = (direction) => {
-  const firstPart = snake.parts[0];
   const lastPart = snake.parts[snake.parts.length - 1];
-  const firstPartX = firstPart.x;
-  const firstPartY = firstPart.y;
+  const firstPart = snake.parts[0];
 
   if (direction === `right`) {
-    moveRight(firstPart, lastPart, firstPartX, firstPartY);
-
-  } else if (snake.direction === `down`) {
-    moveDown(firstPart, lastPart, firstPartX, firstPartY);
-
-  } else if (snake.direction === `left`) {
-    moveLeft(firstPart, lastPart, firstPartX, firstPartY);
-
-
-  } else if (snake.direction === `up`) {
-    moveUp(firstPart, lastPart, firstPartX, firstPartY);
+    lastPart.x = firstPart.x + gameField.step;
+    lastPart.y = firstPart.y + 0;
+  } else if (direction === `down`) {
+    lastPart.x = firstPart.x + 0;
+    lastPart.y = firstPart.y + gameField.step;
+  } else if (direction === `left`) {
+    lastPart.x = firstPart.x - gameField.step;
+    lastPart.y = firstPart.y + 0;
+  } else if (direction === `up`) {
+    lastPart.x = firstPart.x + 0;
+    lastPart.y = firstPart.y - gameField.step;
   }
+
+  snake.parts.unshift(snake.parts.pop());
 };
+
 
 const nextStep = () => {
-  clear(gameCanvas);
-
-  render(gameCanvas);
-
-  // render border
-  ctx.strokeStyle = border.color;
-  ctx.lineWidth = border.size;
-  ctx.strokeRect(gameCanvas.x + border.size / 2, gameCanvas.x + border.size / 2, gameCanvas.width, gameCanvas.height);
-
+  clear(gameField);
+  render(gameField);
   renderSnake(snake);
   render(apple);
 
-  if (checkBumpIntoTail(snake.direction) || checkBumpIntoWall(snake.direction)) {
-    console.log(`Врезался`);
-    overGame();
-  }
-
-
-  // snake movenment
-  checkSnakeMove(snake.direction);
-
-
-  // apple generate
   if (isAppleEaten()) {
-    addNewSnakePart();
-    generateApple();
+    addNewSnakePart(snake);
+    generateApple(apple);
   }
+
+  snakeMove(snake.direction);
 };
 
-const checkKey = (key) => {
+const changeSnakeDirection = (key) => {
   if (key === `ArrowRight`) {
     snake.direction = `right`;
   } else if (key === `ArrowDown`) {
@@ -305,7 +174,7 @@ const checkKey = (key) => {
 };
 
 document.addEventListener(`keydown`, (evt) => {
-  checkKey(evt.key);
+  changeSnakeDirection(evt.key);
 });
 
 startGame();
